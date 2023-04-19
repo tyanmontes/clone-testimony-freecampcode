999829c6ebd50e605ae78001c5521d03e740595c
Sebastian Montes <sebastians2896@gmail.com>
2023-04-19 17:22:06 -0500

    Primeros Cambios

---
+++ src/components/testimony.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Testimony(){
+  return (
+    <div className='testimony-container'>
+      <img 
+        className='testimony-image'
+        src={require('../images/emma-testimony.png')}
+        alt="Emma's Photo"
+      />
+    </div>
+  );
+}
