ed2882647ed8875c3f44509064a890903ac38390
Sebastian Montes <sebastians2896@gmail.com>
2023-04-21 10:51:15 -0500

    segundo commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Testimony from './components/Testimony';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='principal-container'>
+        <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
+        <Testimony />
+      </div>
     </div>
   );
 }
--- src/components/testimony.js
+++ src/components/testimony.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../styles/Testimony.css';
 
 function Testimony(){
   return (
@@ -8,6 +9,13 @@ function Testimony(){
         src={require('../images/emma-testimony.png')}
         alt="Emma's Photo"
       />
+      <div className="testimony-text-container">
+        <p className="testimony-name">Emma Bostian en Suecia</p>
+        <p className="testimony-position">Ingeniera de Sofware en Spotify</p>
+        <p className="testimony-text">"Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify."</p>
+      </div>
     </div>
   );
 }
+
+export default Testimony;
